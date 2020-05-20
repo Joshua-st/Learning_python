d2d65b42a6a26759e30ce7d98afc4c9b72514704
joshua-st <joshua@os.university>
2020-05-20 09:30:56 +0200

    base python crush course

---
+++ .idea/M_lib.py
@@ -0,0 +1,79 @@
+
+
+
+character_name = "Mars"
+character_age = "25"
+character_name1 = "Jupiter"
+character_bd = "23"
+print('')
+print("His name was " + character_name + ", "  + character_name + " was " + character_age + ",")
+print("That boy " + character_name + " loved " + character_name1 + ".")
+
+
+print("" + character_name + " was handsome. ")
+print("" + character_name1 + " was beautiful too.")
+print('')
+print("she was " + character_bd +" years old.")
+print("after some time, they got married")
+print('')
+print('')
+
+
+print(""+character_name + ' and ' + character_name1 + " triangle of realities")
+
+
+print("\  /   \ ")
+print(" \/     \ ")
+print(" /\      \    ")
+print("___\______")
+print("    \ ")
+print("    / \ ")
+print("")
+print("Working with Strings")
+phrase = "\"jehovah god\" loves me"
+print("" + phrase + ", \nYes I know. (CONCATENATION)")
+print(phrase.upper().isupper())
+print("How great thou Art God".upper())
+print(len(phrase))
+print(phrase[5])
+print(phrase.index("v"))
+print(phrase.index("love"))
+print(phrase.replace("jehovah","yahweh"))
+my_fig=-2.8
+print(str(my_fig) + " is a good number")
+print(abs(my_fig))
+print(pow(3,2))
+print(phrase.replace("loves", "cares for"))
+print(phrase.upper())
+print(pow(4,2))
+from math import *
+print(sqrt(100))
+print(round(3.48))
+name = input("Enter your name:")
+age = input("how old are you:")
+need_help = input("get in touch with us: ")
+Customer_contact1 = input("your email:")
+customer_contact2 = input("your phone contact:")
+
+
+num1 = input("Enter number: ")
+num2 = input("Enter another number: ")
+Result = float(num1) * float(num2)
+print(Result)
+print("Hey " + name + "! we are glad to hear from you! we are looking for an employee who is " + age + " ! for more information contact info@os.university. well reach you on " + customer_contact2 + " cheers")
+
+color = input("Enter color: ")
+animal_character = input("Enter favorite animal: ")
+Food = input("Enter favorite food: ")
+Actor = input("Enter your crush actor/actress: ")
+print("For so long, I thought a " + animal_character + " was the most fierce animal in the jungle. For that reason I loved it! I had never seen a  " + color + animal_character + " because  " + Actor + " had said in the last show that a " + animal_character + " ate " + Food + " and got really ill,  which made the the forest stable for 3 days! I never stopped loving " + animal_character + "!")
+
+
+paragraphs = ["learners ", "we love OS.University ", "we get much from it ", "we encourage every learner to engage with the OSU platform ", "You will not regret this ", "do join, you will thank us later ! "  ]
+new_list = ["Enock", "Elijah"]
+print(new_list[0])
+paragraphs.append("John")
+paragraphs.sort()
+print(paragraphs)
+coordinates = (1, 5)
+print(coordinates[0])
